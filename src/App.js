061e794cc5cdaecdea93ba70e2a25d2b4bd78a76
arrchiver <111659777+arrchiver@users.noreply.github.com>
2022-08-23 19:55:18 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,49 +7,42 @@ import "./App.css";
  * Tailwind file is imported in App.css
  */
 
-export default function App() {
+import './App.css';
+
+function App() {
   return (
-    <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
-      <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img
-          className="col-span-2 row-span-3 animate-spin m-auto"
-          style={{ animationDuration: "30s" }}
-          src={logo}
-          alt="React Logo"
-          width="300"
-        />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-      </div>
-
-      <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Welcome to Your New React App{" "}
-        <span className="block text-lg text-blue-400">on DigitalOcean</span>
-      </h1>
-
-      <div className="grid grid-cols-2 grid-rows-2 gap-4">
-        <Button
-          text="DigitalOcean Docs"
-          url="https://www.digitalocean.com/docs/app-platform"
-        />
-        <Button
-          text="DigitalOcean Dashboard"
-          url="https://cloud.digitalocean.com/apps"
-        />
-      </div>
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <Notes />
     </div>
   );
 }
 
-function Button({ className, text, url = "#" }) {
+function Notes() {
+  const backendData = [
+    { title: "Grocery List", description: "Milk, Soup, Bread", createdat: "01-18-2021" },
+    { title: "Math Homework", description: "Remember to finish question 8-10 before monday", createdat: "12-01-2020" },
+    { title: "Call James", description: "Ask him about the company party.", createdat: "12-30-2020" }
+  ]
+
+  const noteRootStyle = {
+    border: "2px #0af solid",
+    borderRadius: 9,
+    margin: 20,
+    backgroundColor: "#efefef",
+    padding: 6
+  };
+
   return (
-    <a
-      href={url}
-      className={`${className} py-3 px-6 bg-purple-400 hover:bg-purple-300 text-purple-800 hover:text-purple-900 block rounded text-center shadow flex items-center justify-center leading-snug text-xs transition ease-in duration-150`}
-    >
-      {text}
-    </a>
-  );
+    <div style={{ width: 400 }}>
+      {backendData.map(ele => 
+        <div style={noteRootStyle}>
+          <h3>{ele.title}</h3>
+          <p>{ele.description}</p>
+          <small>{ele.createdat}</small>
+        </div>
+      )}
+    </div>
+  )
 }
+
+export default App;
